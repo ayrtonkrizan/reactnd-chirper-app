7336e296dadc8e480a242192894a06fb533ce977
AyrtonKrizan <ayrtonkrizan@gmail.com>
2019-07-21 17:56:09 -0300

    feat: Create Dashboard view

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 
+import Dashboard from './Dashboard'
+
 class App extends Component {
   componentDidMount(){
     this.props.dispatch(handleInitialData())
@@ -9,10 +11,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+        {this.props.loading? null : <Dashboard/>}
       </div>
     )
   }
 }
 
-export default connect()(App)
+function mapStateToProps({ authedUser }){
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
