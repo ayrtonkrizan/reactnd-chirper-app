7a26d938fa8d71b084618e369dd217908d678f9b
AyrtonKrizan <ayrtonkrizan@gmail.com>
2019-07-21 20:58:43 -0300

    feat: Add TweetPage

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import LoadingBar from 'react-redux-loading';
-import Dashboard from './Dashboard'
+import Dashboard from './Dashboard';
+import NewTweet from './NewTweet';
+import TweetPage from './TweetPage';
 
 class App extends Component {
   componentDidMount(){
@@ -12,7 +14,7 @@ class App extends Component {
     return (
       <div>
         <LoadingBar/>
-        {this.props.loading? null : <Dashboard/>}
+        {this.props.loading? null : <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}}/>}
       </div>
     )
   }
