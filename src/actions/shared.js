888515fdac1d9290fe5daf5c2faa7fc86ed71c93
AyrtonKrizan <ayrtonkrizan@gmail.com>
2019-07-21 19:11:39 -0300

    feat: Add loading bar from react-redux-loading

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,6 +2,7 @@ import { getInitialData } from '../utils/api';
 import { receiveUsers } from './users';
 import { receiveTweets } from './tweets';
 import { setAuthedUser } from './authedUser';
+import { showLoading, hideLoading} from 'react-redux-loading'
 
 // constant defined to not handle authentication;
 const AUTHED_USER = 'dan_abramov';
@@ -9,11 +10,13 @@ export const RECEIVE_DATA = 'RECEIVE_DATA';
 
 export function handleInitialData(){
     return dispatch => {
+        dispatch(showLoading());
         return getInitialData()
             .then(({users, tweets}) => {
                 dispatch(receiveUsers(users))
                 dispatch(receiveTweets(tweets))
                 dispatch(setAuthedUser(AUTHED_USER))
+                dispatch(hideLoading());
             })
     }
 }
